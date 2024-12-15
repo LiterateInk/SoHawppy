958b3b465f4462ae68e565ca399db2157d29eea7
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-15 04:18:55 +0100

    fix(models): typo

--- examples/find-clients.ts
+++ examples/find-clients.ts
@@ -3,7 +3,7 @@ import { credentials } from "./_credentials";
 
 void async function main () {
   const { token } = await sohappy.login(credentials.mail, credentials.password);
-  const clients = await sohappy.findClients(token.value, "Paris", sohappy.UsageCode.Students);
+  const clients = await sohappy.findClients(token.value, "Paris", sohappy.UsageCode.Student);
 
   console.log(clients);
 }();
--- src/models/atoms/usage-code.ts
+++ src/models/atoms/usage-code.ts
@@ -1,5 +1,5 @@
 export const UsageCode = {
-  Students: "ETUDIANT"
+  Student: "ETUDIANT"
 } as const;
 
 export type UsageCode = typeof UsageCode[keyof typeof UsageCode];
